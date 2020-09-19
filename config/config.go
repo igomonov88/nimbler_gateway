77588f42803ea5dce94505bceea1bb2117e986e8
Igor Gomonov <igor.gomonov@apalon.com>
2020-09-19 18:04:08 +0300

    add config package to read provided config for the gateway service.

---
+++ config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"os"
+	"time"
+
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Web    Web    `yaml:"web"`
+	Zipkin Zipkin `yaml:"zipkin"`
+}
+
+type Web struct {
+	APIHost         string        `yaml:"api_host"`
+	DebugHost       string        `yaml:"debug_host"`
+	ReadTimeout     time.Duration `yaml:"read_timeout"`
+	WriteTimeout    time.Duration `yaml:"write_timeout"`
+	ShutdownTimeout time.Duration `yaml:"shutdown_timeout"`
+}
+
+type Zipkin struct {
+	LocalEndpoint string  `yaml:"local_endpoint"`
+	ReporterURI   string  `yaml:"reporter_uri"`
+	ServiceName   string  `yaml:"service_name"`
+	Probability   float64 `yaml:"probability"`
+}
+
+func Parse(filePath string) (*Config, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, errors.Wrap(err, "reading file path")
+	}
+	defer file.Close()
+	var cfg Config
+	if err := yaml.NewDecoder(file).Decode(&cfg); err != nil {
+		return nil, errors.Wrap(err, "decoding config file")
+	}
+	return &cfg, nil
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module nimbler_gateway
+
+go 1.14
+
+require (
+	github.com/pkg/errors v0.9.1
+	gopkg.in/yaml.v2 v2.3.0
+)
